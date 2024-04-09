e6d491a6b189580c22a27d2bdc86d5d44b1dcb97
VictoriaMik13 <162870814+VictoriaMik13@users.noreply.github.com>
2024-04-09 21:02:01 +0400

    Create Continent.java

---
+++ Continent.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Continent {
+    private String Name;
+    private Arraylist<Territory> territories;
+    private int bonusTroops;
+
+    public Continent(String name, int bonusTroops) {
+	      this.name = name;
+	      this.bonusTroops = bonusTroops;
+	      this.territories = new ArrayList<>();
+    }
+
+    public void addTerritory(Territory territory) {
+	      territories.add(territory);
+    }
+
+    public void removeTerritory(Territory territory) {
+	      territories.remove(territory);
+    }
+
+    public boolean controlsContinent(Player player) {
+	      for(Territory territory : territories) {
+	         if(territory.getOwner() != player) {
+	           return false;
+	         }
+	      } 
+	      return true;
+    }
+    
+}
