59ed5e50c7852a6d2c07278f142fccf808d2ebba
VictoriaMik13 <162870814+VictoriaMik13@users.noreply.github.com>
2024-04-09 21:04:03 +0400

    Create ArmyPiece.java

---
+++ ArmyPiece.java
@@ -0,0 +1,9 @@
+public class ArmyPiece {
+    private String pieceType;
+    private numberOfArmies;
+
+    public ArmyPiece(String pieceType) {
+	      this.pieceType = pieceType;
+	      this.numberOfArmies = numberOfArmies;
+    }
+}
